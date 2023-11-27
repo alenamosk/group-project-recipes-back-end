758564e6b2a19ba480dd23561bd857fb93eecb3d
alenamosk <almoskaeva@gmail.com>
2023-11-27 13:32:07 +0100

    basic sevrer part and schema done

---
+++ server.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import { PrismaClient } from "@prisma/client";
+import { json } from "express";
+
+// Create an express app
+const app = express();
+
+// Tell the app to allow json in the request body
+app.use(json());
+
+const port = 3002;
+
+// Create a prisma client
+const prisma = new PrismaClient();
+
+// Your routes go underneath here
+
+app.get("/recepies", (req, res) => {
+  res.send("Hello, world!");
+});
+
+app.listen(port, () => {
+  console.log(`⚡ Server listening on port: ${port}`);
+});

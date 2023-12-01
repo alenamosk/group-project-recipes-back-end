89ae11044370fb64e231b761f2e9bd81c69c4833
Shruti Jain <hrm.shruti@gmail.com>
2023-12-01 14:13:20 +0100

    changed in Prisma model-made changes in the relationship in json file-seed file-created the end point for login page

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,32 +1,76 @@
-import recipe from "./data/recipes.json";
-import comment from "./data/comments.json";
-import categorie from "./data/categories.json";
+import recipe from './data/recipes.json';
+import comment from './data/comments.json';
+import categorie from './data/categories.json';
+import user from './data/users.json';
 
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-const seed = async () => {
+const seedCategoriesAndUsers = async () => {
   for (let i = 0; i < categorie.length; i += 1) {
     const categorieData = categorie[i];
     if (categorieData) {
-      await prisma.category.create({ data: categorieData });
+      try {
+        await prisma.category.create({ data: categorieData });
+      } catch (error) {
+        console.error('Error seeding category:', error);
+      }
     }
   }
 
+  for (let i = 0; i < user.length; i += 1) {
+    const usertData = user[i];
+    if (usertData) {
+      await prisma.user.create({ data: usertData });
+    }
+  }
+};
+
+const seedRecipes = async () => {
   for (let i = 0; i < recipe.length; i += 1) {
     const recipeData = recipe[i];
     if (recipeData) {
-      await prisma.recipe.create({ data: recipeData });
+      const categoryIds = Array.isArray(recipeData.categories)
+        ? recipeData.categories.map((category) => category.categoryId)
+        : [];
+
+      try {
+        await prisma.recipe.create({
+          data: {
+            id: recipeData.id,
+            name: recipeData.name,
+            categories: {
+              connect: categoryIds.map((categoryId) => ({ id: categoryId })),
+            },
+            img_url: recipeData.img_url,
+            instructions: recipeData.instructions,
+            ingredients: recipeData.ingredients,
+            prep_time: recipeData.prep_time,
+            serves: recipeData.serves,
+            userId: recipeData.userId,
+          },
+        });
+      } catch (error) {
+        console.error('Error seeding recipe:', error);
+      }
     }
   }
+};
 
+const seedComments = async () => {
   for (let i = 0; i < comment.length; i += 1) {
     const commentData = comment[i];
     if (commentData) {
       await prisma.comment.create({ data: commentData });
     }
   }
+};
+
+const seed = async () => {
+  await seedCategoriesAndUsers();
+  await seedRecipes();
+  await seedComments();
 };
 
 seed();
